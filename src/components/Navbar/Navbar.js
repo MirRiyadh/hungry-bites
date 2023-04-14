6b286a5a83e647bcf9e2334fc1892dc3d8d819ca
Riyadh <riyadh.ju@gmail.com>
2023-04-14 22:18:44 +0600

    added navbar styles with mobile view

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+
+import "./Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import {
+  MdOutlineRestaurantMenu,
+  MdoutlineRestaurantMenu,
+} from "react-icons/md";
+
+import images from "../../constants/images";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <nav className="app_navbar">
+      <div className="app_navbar-logo">
+        <img src={images.gericht} alt="logo" />
+      </div>
+
+      <ul className="app_navbar-links">
+        <li className="p__opensans">
+          <a href="#home">Home</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#home">About</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#home">Menu</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#home">About us</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#home">Contact</a>
+        </li>
+      </ul>
+
+      <div className="app_navbar-login">
+        <a href="#login" className="p__opensans">
+          Log In / Registration
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book Table
+        </a>
+      </div>
+
+      <div className="app_navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
+
+        {toggleMenu && (
+          <div className="app_navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay_close"
+              onClick={() => setToggleMenu(false)}
+            />
+
+            <ul className="app_navbar-smallscreen_links">
+              <li className="p__opensans">
+                <a href="#home">Home</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#home">About</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#home">Menu</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#home">About us</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#home">Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
