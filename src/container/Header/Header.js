75c94333c37a637b002f0b3715392c68da7b88f7
Riyadh <riyadh.ju@gmail.com>
2023-04-17 21:38:44 +0600

    added chnage text on header

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import SubHeading from "../../components/SubHeading/SubHeading";
+import { images } from "../../constants";
 
 const Header = () => {
   return (
-    <div className="app__header app__wrapper section__padding" id="home">
-      <div className="app__wrapper_info">
+    <div className="app_header app__wrapper section__padding" id="home">
+      <div className="app_wrapper_info">
         <SubHeading title="Chase the new flavour" />
         <h1 className="app__header-h1">The Key To Fine Dining</h1>
         <p className="p__opensans" style={{ margin: "2rem 0" }}>
@@ -16,7 +18,7 @@ const Header = () => {
         </button>
       </div>
 
-      <div className="app__wrapper_img">
+      <div className="app_wrapper_img">
         <img src={images.welcome} alt="header_img" />
       </div>
     </div>
