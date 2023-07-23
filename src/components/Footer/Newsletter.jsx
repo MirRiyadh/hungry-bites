df8ada43b95c5b1de0c3d0fd97f6b8b26d60d2d5
Farna Shafrina <riyadh.ju@gmail.com>
2023-07-23 23:08:32 +0600

    added form

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -2,6 +2,13 @@ import React from "react";
 
 import "./Newsletter.css";
 
-const Newsletter = () => <div>Newsletter and signup page</div>;
+const Newsletter = () => (
+  <div>
+    <h1>Newsletter and signup page</h1>
+    <form action="#">
+      <input type="text" />
+    </form>
+  </div>
+);
 
 export default Newsletter;
