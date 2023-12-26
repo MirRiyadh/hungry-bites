a89e62e2f63e4bdb0491f827e8c83c9eab077290
MirRiyadh <riyadh.ju@gmail.com>
2023-12-26 23:05:16 +0600

    update text

--- src/components/Menuitem/MenuItem.js
+++ src/components/Menuitem/MenuItem.js
@@ -8,6 +8,12 @@ const MenuItem = () => {
         <li>test 2</li>
         <li>test 3</li>
         <li>test 4</li>
+        <li>test 4</li>
+        <li>test 4</li>
+        <li>test 4</li>
+        <li>test 4</li>
+        <li>test 4</li>
+        <li>test 4</li>
         
       </ul>
     </div>
